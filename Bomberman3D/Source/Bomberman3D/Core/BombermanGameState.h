9b33fe986724f14661750c5a5a3500f96b6d456d
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-03-12 12:28:49 +0100

    chore: create bombermangamestate class

---
+++ Bomberman3D/Source/Bomberman3D/Core/BombermanGameState.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2026, Michal Flaška & RedFox Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "BombermanGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN3D_API ABombermanGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
